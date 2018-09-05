a78a4266db2946036709ff8cfe1af613d314caca
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-05 20:58:58 +0530

    Update 01Writing_File.py

--- 22Python File Handling/01Writing_File.py
+++ 22Python File Handling/01Writing_File.py
@@ -1,5 +1,5 @@
-f = open('myfile.txt', 'w')    # open file for writing
-f.write('this first line\n')   # write a line to the file
-f.write('this second line\n')  # write one more line to the file
+f = open('test.txt', 'w')    # open file for writing
+f.write('line one\n')   # write a line to the file
+f.write('line two\n')  # write one more line to the file
 f.close()  # close the file
 print("Write Successful") 
