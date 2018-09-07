90c5718c1a883cb7c969fa269a06bb5ed35e0401
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-07 11:14:38 +0530

    Update 01Data_Types.py

--- 04Datatype & Variables/01Data_Types.py
+++ 04Datatype & Variables/01Data_Types.py
@@ -2,7 +2,7 @@ a = 100  # a is integer
 pie = 3.14  # pie is float
 name = "python is great"  # name is string
 
-a = b = c = 200  # this statement assign 200 to z, y and x.
+x = y = z = 200  # this statement assign 200 to z, y and x.
 
 print(a)
 print(pie)
