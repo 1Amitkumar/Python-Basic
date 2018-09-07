ba5ebf27b80711b63976f8a8110f59dfdbf231d4
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-07 11:39:44 +0530

    Update 05Slicing_List.py

--- 07Python Lists/05Slicing_List.py
+++ 07Python Lists/05Slicing_List.py
@@ -1,5 +1,5 @@
 list = [11, 33, 44, 66, 788, 1]
-print(list[0:5])  # this will return list starting from index 0 to index 4
+print(list[0:5])  # return list starting from index 0 to index 4
 
 print(list[:3])
 
