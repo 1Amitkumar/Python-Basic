7da705eeefebf1a02e6916361eb446b5a6729668
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-10 11:38:51 +0530

    Update 01InBuilt_Mathematical_Function.py

--- 18Python Mathematical Function/01InBuilt_Mathematical_Function.py
+++ 18Python Mathematical Function/01InBuilt_Mathematical_Function.py
@@ -1,13 +1,13 @@
-print(abs(-22))             # Returns the absolute value
+print(abs(-22))             # Returns absolute value
 
-print(max(9, 3, 12, 81))      # Returns the maximum number
+print(max(9, 3, 12, 81))      # Returns maximum number
 
-print(min(78, 99, 12, 32))   # Returns the minimum number
+print(min(78, 99, 12, 32))   # Returns minimum number
 
-print(pow(8, 2))            # can also be written as 8 ** 2
+print(pow(8, 2))            # can also be 8 ** 2
 
-print(pow(4.1, 3.2))        # can also be written as 4.1 ** 3.2
+print(pow(4.1, 3.2))        # can also be 4.1 ** 3.2
 
-print(round(5.32))           # Rounds to its nearest integer
+print(round(5.32))           # Rounds to nearest integer
 
 print(round(3.1456875712, 3))  # Return number with 3 digits after decimal point
