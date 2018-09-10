03d82b63de1b0afc8f115e2bf29bdd6451b29f2c
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-10 11:35:01 +0530

    Update 13Positional And Keyword_Argument.py

--- 17Python Functions/13Positional And Keyword_Argument.py
+++ 17Python Functions/13Positional And Keyword_Argument.py
@@ -1,15 +1,15 @@
-def my_func(a, b, c):
+def myfunc(a, b, c):
     print(a, b, c)
 
 
 # using positional arguments only
-my_func(12, 13, 14)
+myfunc(12, 13, 14)
 
 # here first argument is passed as positional arguments while other two as keyword argument
-my_func(12, b=13, c=14)
+myfunc(12, b=13, c=14)
 
 # same as above
-my_func(12, c=13, b=14)
+myfunc(12, c=13, b=14)
 
 # this is wrong as positional argument must appear before any keyword argument
-# my_func(12, b=13, 14)
+# myfunc(12, b=13, 14)
