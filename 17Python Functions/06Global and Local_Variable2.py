a6719f96180b6b4c9e4d223d5abd349243aeb44a
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-10 11:31:55 +0530

    Update 06Global and Local_Variable2.py

--- 17Python Functions/06Global and Local_Variable2.py
+++ 17Python Functions/06Global and Local_Variable2.py
@@ -1,11 +1,11 @@
 xy = 100
 
 
-def cool():
+def func():
     xy = 200  # xy inside the function is totally different from xy outside the function
     print(xy)  # this will print local xy variable i.e 200
 
 
-cool()
+func()
 
 print(xy)  # this will print global xy variable i.e 100
