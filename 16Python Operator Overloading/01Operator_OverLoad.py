70ba1a51efbeaf08d6f8dbb7e937dc6061f8caf9
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-10 11:26:14 +0530

    Update 01Operator_OverLoad.py

--- 16Python Operator Overloading/01Operator_OverLoad.py
+++ 16Python Operator Overloading/01Operator_OverLoad.py
@@ -1,7 +1,7 @@
 import math
 
 
-class Circle:
+class Circle_Detail:
 
     def __init__(self, radius):
         self.__radius = radius
@@ -16,14 +16,14 @@ class Circle:
         return math.pi * self.__radius ** 2
 
     def __add__(self, another_circle):
-        return Circle(self.__radius + another_circle.__radius)
+        return Circle_Detail(self.__radius + another_circle.__radius)
 
 
-c1 = Circle(4)
+c1 = Circle_Detail(4)
 print(c1.get_radius())
 
-c2 = Circle(5)
+c2 = Circle_Detail(5)
 print(c2.get_radius())
 
-c3 = c1 + c2  # This became possible because we have overloaded + operator by adding a    method named __add__
+c3 = c1 + c2  # This is possible because we have overloaded + operator by adding a    method named __add__
 print(c3.get_radius())
