c09f53db14d3f970822b62b8012fa7cba28f011f
AMIT KUMAR <38284250+1Amitkumar@users.noreply.github.com>
2018-09-10 10:44:28 +0530

    Update 03Add or Modify_Elements.py

--- 09Python Dictionaries/03Add or Modify_Elements.py
+++ 09Python Dictionaries/03Add or Modify_Elements.py
@@ -1,9 +1,9 @@
-friends = {
-'tom' : '111-222-333',
-'jerry' : '666-33-111'
+frnd = {
+'ram' : '111-222-333',
+'sita' : '666-33-111'
 }
 
-friends['bob'] = '888-999-666'
-print(friends)
+frnd['bob'] = '888-999-666'
+print(frnd)
 
 
